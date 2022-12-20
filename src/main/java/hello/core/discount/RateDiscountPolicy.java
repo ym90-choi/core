164b68ae5cb46be2e9fba5e2c5b4504b1654a68d
YeongMan Choi <ym90.choi@samsung.com>
2022-12-20 11:18:30 +0900

    Add Qualifier annotation in OrderServiceImpl class

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,16 +2,18 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("rateDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountRate = 10; // 10% 할인
 
     @Override
     public int discount(Member member, int price) {
-        if (member.getGrade() == Grade.VIP) {
+        if(member.getGrade() == Grade.VIP) {
             return price * discountRate / 100;
         } else {
             return 0;
