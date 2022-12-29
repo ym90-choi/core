f6a779d381ac366f64ba7e4a7ff80ce4015995f0
YeongMan Choi <ym90.choi@samsung.com>
2022-12-29 12:17:00 +0900

    Add MainDiscountPolicy class

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,12 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("rateDiscountPolicy")
+//@Qualifier("rateDiscountPolicy")
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountRate = 10; // 10% 할인
